5d3ee8f64462048ff696ca8ce6a015c45fbd39c7
Luke Plaster <me@lukep.org>
2017-09-30 23:04:33 -0500

    Targeting netstandard1.6 when building the framework dll

--- neon.sh
+++ neon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DIR=`pwd`/`dirname $0`
-dotnet build -c Release ${DIR}/Neo.Ecosystem/src/neo-devpack-dotnet/Neo.SmartContract.Framework/Neo.SmartContract.Framework.csproj
+dotnet build -f netstandard1.6 -c Release ${DIR}/Neo.Ecosystem/src/neo-devpack-dotnet/Neo.SmartContract.Framework/Neo.SmartContract.Framework.csproj
 cp ${DIR}/Neo.Ecosystem/src/neo-devpack-dotnet/Neo.SmartContract.Framework/bin/Release/netstandard1.6/Neo.SmartContract.Framework.dll ./
 dotnet run -p ${DIR}/Neo.Ecosystem/src/neo-compiler/neon/neon.csproj -c Release $1
